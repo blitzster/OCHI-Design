07c550401123a1bed5851ebb05c0cbf086a6370c
Akshay S <blitz122333@gmail.com>
2024-07-09 22:12:13 +0530

    started landing page

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function LandingPage() {
+    return (
+        <div className='w-full h-screen bg-zinc-900 pt-1'>
+            <div className='textstructure mt-32 px-20'>
+                <div className='masker'>
+                    <h1 className="uppercase text-9xl leading-none tracking-tighter font-medium font-['Founders_Grotesk']">We Create</h1>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default LandingPage
